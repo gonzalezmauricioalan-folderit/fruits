802090634ad1efd1144be7b193df45737bd13c99
Alan Mauricio Gonzalez <alangonzalez@folderit.net>
2021-11-04 14:03:41 -0300

    icons

--- pages/CrudFruits.js
+++ pages/CrudFruits.js
@@ -1,6 +1,10 @@
 import Link from 'next/link'
 import { useCollection } from 'react-firebase-hooks/firestore'
 import firebase from '../firebase/clientApp'
+import { FaTrash } from 'react-icons/fa';
+import { FaPen } from 'react-icons/fa';
+import { FaPlus } from 'react-icons/fa';
+
 
 const CrudFruits = () => {
     const [fruits, fruitsLoading, error] = useCollection(firebase.firestore().collection('fruits'), {})
@@ -22,8 +26,8 @@ const CrudFruits = () => {
                             <th>{fruit.data().name}</th>
                             <th>{fruit.data().emoji}</th>
                             <th>
-                                <button>X</button>
-                                <button>U</button>
+                                <button><FaTrash style={{color:'#C70000'}}/></button>
+                                <button><FaPen style={{color: '#6e8c91'}}/></button>
                             </th>
                         </tr>
                     )
@@ -32,7 +36,7 @@ const CrudFruits = () => {
                 <tfoot>
                     <tr>
                         <th>
-                            <button>+</button>
+                            <button><FaPlus style={{color: '#6e8c91'}}/></button>
                         </th>
                     </tr>
                 </tfoot>
