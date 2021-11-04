01a1ddf91ceee9936fd447dfe122ebadc7059758
Alan Mauricio Gonzalez <alangonzalez@folderit.net>
2021-11-04 11:17:10 -0300

    if the user is not logged show login component

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import firebase from "../firebase/clientApp";
 
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { useCollection } from 'react-firebase-hooks/firestore'
+import Auth from '../components/Auth';
 
 
 export default function Home() {
@@ -19,24 +20,30 @@ export default function Home() {
   }
   return (
     <>
-      <div className={styles.container}>
-        <h1>Choice your favorite fruit</h1>
-        <button onClick={() => addVoteDocument('Watermelon')} className={styles.btn}>🍉</button>
-        <button onClick={() => addVoteDocument('Tangerine')} className={styles.btn}>🍊</button>
-        <button onClick={() => addVoteDocument('Banana')} className={styles.btn}>🍌</button>
-        <button onClick={() => addVoteDocument('Mango')} className={styles.btn}>🥭</button>
-        <button onClick={() => addVoteDocument('Grapes')} className={styles.btn}>🍇</button>
-        <button onClick={() => addVoteDocument('Red Apple')} className={styles.btn}>🍎</button>
-      </div>
-      <div>
-        <h2>Votes:</h2>
-        <h4>Watermelon: {votes?.docs?.filter(d => d.data().vote === 'Watermelon').length}</h4>
-        <h4>Tangerine:  {votes?.docs?.filter(d => d.data().vote === 'Tangerine').length}</h4>
-        <h4>Banana:  {votes?.docs?.filter(d => d.data().vote === 'Banana').length}</h4>
-        <h4>Mango:  {votes?.docs?.filter(d => d.data().vote === 'Mango').length}</h4>
-        <h4>Grapes:  {votes?.docs?.filter(d => d.data().vote === 'Grapes').length}</h4>
-        <h4>Red Apple: {votes?.docs?.filter(d => d.data().vote === 'Red Apple').length}</h4>
-      </div>
+      {loading && <h4>Loading...</h4>}
+      {!user && <Auth />}
+
+      {user && (
+        <>
+          <div className={styles.container}>
+            <h1>Choice your favorite fruit</h1>
+            <button onClick={() => addVoteDocument('Watermelon')} className={styles.btn}>🍉</button>
+            <button onClick={() => addVoteDocument('Tangerine')} className={styles.btn}>🍊</button>
+            <button onClick={() => addVoteDocument('Banana')} className={styles.btn}>🍌</button>
+            <button onClick={() => addVoteDocument('Mango')} className={styles.btn}>🥭</button>
+            <button onClick={() => addVoteDocument('Grapes')} className={styles.btn}>🍇</button>
+            <button onClick={() => addVoteDocument('Red Apple')} className={styles.btn}>🍎</button>
+          </div>
+          <div>
+            <h2>Votes:</h2>
+            <h4>Watermelon: {votes?.docs?.filter(d => d.data().vote === 'Watermelon').length}</h4>
+            <h4>Tangerine:  {votes?.docs?.filter(d => d.data().vote === 'Tangerine').length}</h4>
+            <h4>Banana:  {votes?.docs?.filter(d => d.data().vote === 'Banana').length}</h4>
+            <h4>Mango:  {votes?.docs?.filter(d => d.data().vote === 'Mango').length}</h4>
+            <h4>Grapes:  {votes?.docs?.filter(d => d.data().vote === 'Grapes').length}</h4>
+            <h4>Red Apple: {votes?.docs?.filter(d => d.data().vote === 'Red Apple').length}</h4>
+          </div>
+        </>)}
     </>
   )
 }
