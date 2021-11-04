6a67a15d97065ffb20de79c4acb0311c60def45c
Alan Mauricio Gonzalez <alangonzalez@folderit.net>
2021-11-04 09:38:32 -0300

    connection to firestore

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,15 @@ import styles from '../styles/Home.module.css'
 import firebase from "../firebase/clientApp";
 
 import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollection } from 'react-firebase-hooks/firestore'
 
 export default function Home() {
   const [user, loading, error] = useAuthState(firebase.auth())
-  console.log('user :>> ', user);
-  console.log('loading :>> ', loading);
+  const [votes, votesLoading, votesError] = useCollection(firebase.firestore().collection('votes'), {})
+  console.log(`Loading: ${loading}, user: ${user}`);
+  if (!votesLoading && votes) {
+    votes.docs.map((doc) => console.log(doc.data()));
+  }
   return (
     <div className={styles.container}>
       <h1>Choice your favorite fruit</h1>
