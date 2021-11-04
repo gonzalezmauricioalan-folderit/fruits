4268180d7b1a045177d7ad05d32f33a3fcae0373
Alan Mauricio Gonzalez <alangonzalez@folderit.net>
2021-11-04 11:03:26 -0300

    function to get user information

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +8,9 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+admin.initializeApp();
+const db = admin.firestore();
+
+exports.createdUserDocument = functions.auth.user().onCreate((user) => {
+  db.collection("user").doc(user.uid).set(JSON.parse(JSON.stringify(user)));
+});
