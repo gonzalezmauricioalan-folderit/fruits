a26b654c3324e0d99cb8d40d4aa4702d4094066d
Alan Mauricio Gonzalez <alangonzalez@folderit.net>
2021-11-04 09:48:50 -0300

    vote uploaded

--- pages/index.js
+++ pages/index.js
@@ -4,22 +4,28 @@ import firebase from "../firebase/clientApp";
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { useCollection } from 'react-firebase-hooks/firestore'
 
+
 export default function Home() {
   const [user, loading, error] = useAuthState(firebase.auth())
   const [votes, votesLoading, votesError] = useCollection(firebase.firestore().collection('votes'), {})
   console.log(`Loading: ${loading}, user: ${user}`);
+
+  const db = firebase.firestore();
+  const addVoteDocument = async (vote) => {
+    await db.collection('votes').doc(user.uid).set({ vote })
+  }
   if (!votesLoading && votes) {
     votes.docs.map((doc) => console.log(doc.data()));
   }
   return (
     <div className={styles.container}>
       <h1>Choice your favorite fruit</h1>
-      <button className={styles.btn}>🍉</button>
-      <button className={styles.btn}>🍊</button>
-      <button className={styles.btn}>🍌</button>
-      <button className={styles.btn}>🥭</button>
-      <button className={styles.btn}>🍇</button>
-      <button className={styles.btn}>🍎</button>
+      <button onClick={() => addVoteDocument('Watermelon')} className={styles.btn}>🍉</button>
+      <button onClick={() => addVoteDocument('Tangerine')} className={styles.btn}>🍊</button>
+      <button onClick={() => addVoteDocument('Banana')} className={styles.btn}>🍌</button>
+      <button onClick={() => addVoteDocument('Mango')} className={styles.btn}>🥭</button>
+      <button onClick={() => addVoteDocument('Grapes')} className={styles.btn}>🍇</button>
+      <button onClick={() => addVoteDocument('Red Apple')} className={styles.btn}>🍎</button>
     </div>
   )
 }
