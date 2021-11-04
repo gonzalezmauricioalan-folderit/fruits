010f451e38d6d691ba79461f2b4b8e212c4e3a51
Alan Mauricio Gonzalez <alangonzalez@folderit.net>
2021-11-04 14:59:08 -0300

    create fruit

--- components/VoterList.js
+++ components/VoterList.js
@@ -6,6 +6,7 @@ import Image from 'next/image'
 function VoterList({ id, vote }) {
 
     const [value, loading, error] = useDocument(firebase.firestore().doc(`user/${id}`))
+   
 
     if (loading) {
         return (
@@ -17,7 +18,7 @@ function VoterList({ id, vote }) {
         return null
     }
     return (
-        <div key={id} style={{display:'flex',  flexDirection:'row', justifyContent:'space-around'}}>
+        <div key={id} style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
             <h4>{value.data().displayName}</h4>
             <Image src={value.data().photoURL} width="35rem" height="20rem" alt="user image" />
             <h4>voted: {vote}</h4>
--- pages/CrudFruits.js
+++ pages/CrudFruits.js
@@ -1,13 +1,25 @@
 import Link from 'next/link'
+
 import { useCollection } from 'react-firebase-hooks/firestore'
 import firebase from '../firebase/clientApp'
+
 import { FaTrash } from 'react-icons/fa';
 import { FaPen } from 'react-icons/fa';
-import { FaPlus } from 'react-icons/fa';
 
+import { useState } from 'react';
 
 const CrudFruits = () => {
+    const db = firebase.firestore()
+    const [newFruitName, setNewFruitName] = useState('')
+    const [newFruitEmoji, setNewFruitEmoji] = useState('')
+    const addNewFruit = async (e) => {
+        e.preventDefault()
+        const a = await db.collection('fruits').add({emoji: newFruitEmoji, name: newFruitName })
+        setNewFruitEmoji('')
+        setNewFruitName('')
+    }
     const [fruits, fruitsLoading, error] = useCollection(firebase.firestore().collection('fruits'), {})
+
     return (
         <div>
             <table>
@@ -21,26 +33,32 @@ const CrudFruits = () => {
                 <tbody>
                     {fruits?.docs?.map(fruit =>
                     (
-
                         <tr key={fruit.id}>
                             <th>{fruit.data().name}</th>
                             <th>{fruit.data().emoji}</th>
                             <th>
-                                <button><FaTrash style={{color:'#C70000'}}/></button>
-                                <button><FaPen style={{color: '#6e8c91'}}/></button>
+                                <button><FaTrash style={{ color: '#C70000' }} /></button>
+                                <button><FaPen style={{ color: '#6e8c91' }} /></button>
                             </th>
                         </tr>
                     )
                     )}
                 </tbody>
-                <tfoot>
-                    <tr>
-                        <th>
-                            <button><FaPlus style={{color: '#6e8c91'}}/></button>
-                        </th>
-                    </tr>
-                </tfoot>
             </table>
+            <form onSubmit={addNewFruit}>
+                <label htmlFor="newFruitName"> Fruit Name</label>
+                <input
+                    type="text"
+                    name="newFruitName" id="newFruitName" onChange={(e) => setNewFruitName(e.target.value)}
+                    value={newFruitName} />
+                <label htmlFor="newFruitEmoji">Fruit Emoji</label>
+                <input
+                    type="text"
+                    name="newFruitEmoji"
+                    id="newFruitEmoji" onChange={(e) => setNewFruitEmoji(e.target.value)}
+                    value={newFruitEmoji} />
+                <input type="submit" value="Save Fruit" />
+            </form>
             <Link href="/">
                 <a >&#60;- Back to home</a>
             </Link>
