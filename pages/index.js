17c4fefd9536d7916f6a267586057cff879b453c
Alan Mauricio Gonzalez <alangonzalez@folderit.net>
2021-11-04 10:12:54 -0300

    show votes

--- pages/index.js
+++ pages/index.js
@@ -18,14 +18,25 @@ export default function Home() {
     votes.docs.map((doc) => console.log(doc.data()));
   }
   return (
-    <div className={styles.container}>
-      <h1>Choice your favorite fruit</h1>
-      <button onClick={() => addVoteDocument('Watermelon')} className={styles.btn}>🍉</button>
-      <button onClick={() => addVoteDocument('Tangerine')} className={styles.btn}>🍊</button>
-      <button onClick={() => addVoteDocument('Banana')} className={styles.btn}>🍌</button>
-      <button onClick={() => addVoteDocument('Mango')} className={styles.btn}>🥭</button>
-      <button onClick={() => addVoteDocument('Grapes')} className={styles.btn}>🍇</button>
-      <button onClick={() => addVoteDocument('Red Apple')} className={styles.btn}>🍎</button>
-    </div>
+    <>
+      <div className={styles.container}>
+        <h1>Choice your favorite fruit</h1>
+        <button onClick={() => addVoteDocument('Watermelon')} className={styles.btn}>🍉</button>
+        <button onClick={() => addVoteDocument('Tangerine')} className={styles.btn}>🍊</button>
+        <button onClick={() => addVoteDocument('Banana')} className={styles.btn}>🍌</button>
+        <button onClick={() => addVoteDocument('Mango')} className={styles.btn}>🥭</button>
+        <button onClick={() => addVoteDocument('Grapes')} className={styles.btn}>🍇</button>
+        <button onClick={() => addVoteDocument('Red Apple')} className={styles.btn}>🍎</button>
+      </div>
+      <div>
+        <h2>Votes:</h2>
+        <h4>Watermelon: {votes?.docs?.filter(d => d.data().vote === 'Watermelon').length}</h4>
+        <h4>Tangerine:  {votes?.docs?.filter(d => d.data().vote === 'Tangerine').length}</h4>
+        <h4>Banana:  {votes?.docs?.filter(d => d.data().vote === 'Banana').length}</h4>
+        <h4>Mango:  {votes?.docs?.filter(d => d.data().vote === 'Mango').length}</h4>
+        <h4>Grapes:  {votes?.docs?.filter(d => d.data().vote === 'Grapes').length}</h4>
+        <h4>Red Apple: {votes?.docs?.filter(d => d.data().vote === 'Red Apple').length}</h4>
+      </div>
+    </>
   )
 }
