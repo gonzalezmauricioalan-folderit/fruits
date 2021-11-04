46e4204feb640fe75e67621ec777e0d0bad64a46
Alan Mauricio Gonzalez <alangonzalez@folderit.net>
2021-11-04 09:28:37 -0300

    buttons in index

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import firebase from "../firebase/clientApp";
+
+import { useAuthState } from 'react-firebase-hooks/auth'
 
 export default function Home() {
+  const [user, loading, error] = useAuthState(firebase.auth())
+  console.log('user :>> ', user);
+  console.log('loading :>> ', loading);
   return (
     <div className={styles.container}>
       <h1>Choice your favorite fruit</h1>
+      <button className={styles.btn}>🍉</button>
+      <button className={styles.btn}>🍊</button>
+      <button className={styles.btn}>🍌</button>
+      <button className={styles.btn}>🥭</button>
+      <button className={styles.btn}>🍇</button>
+      <button className={styles.btn}>🍎</button>
     </div>
   )
 }
